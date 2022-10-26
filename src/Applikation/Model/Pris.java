9327e2ae4b1d69954ed346e81dfc069d2e428ee7
Behnke94 <behnke94@hotmail.com>
2022-10-26 12:10:50 +0200

    Opdatering

---
+++ src/Applikation/Model/Pris.java
@@ -0,0 +1,41 @@
+package Applikation.Model;
+
+public class Pris {
+
+    private int pris;
+    private int antalKlip;
+    Produkt produkt;
+
+    public Pris(int pris, int antalKlip, Produkt produkt) {
+        this.pris = pris;
+        this.antalKlip = antalKlip;
+        this.produkt = produkt;
+    }
+
+
+    public Produkt getProdukt() {
+        return produkt;
+    }
+
+
+    public int getPris() {
+        return pris;
+    }
+
+    public void setPris(int pris) {
+        this.pris = pris;
+    }
+
+    public int getAntalKlip() {
+        return antalKlip;
+    }
+
+    public void setAntalKlip(int antalKlip) {
+        this.antalKlip = antalKlip;
+    }
+
+
+    public void setProdukt(Produkt produkt) {
+        this.produkt = produkt;
+    }
+}
