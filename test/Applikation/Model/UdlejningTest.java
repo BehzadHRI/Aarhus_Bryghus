5f62251daeabf19e847a7a718654b97006954790
sent3z <abduuace@gmail.com>
2022-11-03 12:32:38 +0100

    flere test

--- test/Applikation/Model/UdlejningTest.java
+++ test/Applikation/Model/UdlejningTest.java
@@ -1,10 +1,19 @@
 package Applikation.Model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UdlejningTest {
+    private Kunde k;
+
+    @BeforeEach
+
+    void setUP(){
+        k = new Kunde()
+    }
+
 
     @Test
     void setKunde() {
